34093977bda6075abe2f74f2e2bcd4671c94d81f
mralexxp <fl.alexaxp@gmail.com>
2024-12-18 09:54:03 +0500

    first commit

--- 6/99_hw/db_explorer/db_explorer.go
+++ 6/99_hw/db_explorer/db_explorer.go
@@ -1,4 +1,22 @@
 package main
 
-// тут вы пишете код
-// обращаю ваше внимание - в этом задании запрещены глобальные переменные
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"net/http"
+)
+
+type Explorer struct {
+	DB *sql.DB
+}
+
+func NewDbExplorer(db *sql.DB) (http.Handler, error) {
+	e := &Explorer{DB: db}
+	return e, nil
+}
+
+func (e *Explorer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(r.URL.Path + "\n"))
+	w.Write([]byte(r.URL.Query().Get("admin") + "\n"))
+
+}
--- 6/99_hw/db_explorer/main.go
+++ 6/99_hw/db_explorer/main.go
@@ -14,8 +14,8 @@ var (
 	// DSN это соединение с базой
 	// вы можете изменить этот на тот который вам нужен
 	// docker run -p 3306:3306 -v $(PWD):/docker-entrypoint-initdb.d -e MYSQL_ROOT_PASSWORD=1234 -e MYSQL_DATABASE=golang -d mysql
-	// DSN = "root@tcp(localhost:3306)/golang2017?charset=utf8"
-	DSN = "coursera:5QPbAUufx7@tcp(localhost:3306)/coursera?charset=utf8"
+	DSN = "root:1234@tcp(localhost:3306)/photolist?charset=utf8"
+	//DSN = "photolist:love@tcp(localhost:3306)/photolist?charset=utf8"
 )
 
 func main() {
