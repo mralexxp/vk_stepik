6ed32d355d2f184c93f9e0702b204a89308c9232
mralexxp <fl.alexaxp@gmail.com>
2024-12-19 10:58:39 +0500

    end collect struct db

--- 6/99_hw/db_explorer/db_explorer.go
+++ 6/99_hw/db_explorer/db_explorer.go
@@ -1,22 +1,5 @@
 package main
 
 import (
-	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
-	"net/http"
 )
-
-type Explorer struct {
-	DB *sql.DB
-}
-
-func NewDbExplorer(db *sql.DB) (http.Handler, error) {
-	e := &Explorer{DB: db}
-	return e, nil
-}
-
-func (e *Explorer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(r.URL.Path + "\n"))
-	w.Write([]byte(r.URL.Query().Get("admin") + "\n"))
-
-}
---
+++ 6/99_hw/db_explorer/service.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"database/sql"
+	"net/http"
+)
+
+type Explorer struct {
+	DB     *sql.DB
+	Struct map[string]map[string]Column
+}
+
+type Column struct {
+	Field     string      `sql:"Field"`
+	Type      string      `sql:"Type"`
+	Null      string      `sql:"Null"`
+	Key       string      `sql:"Key"`
+	Default   interface{} `sql:"Default"` // Пока не придумал как использовать
+	Increment string      `sql:"increment"`
+}
+
+func NewDbExplorer(db *sql.DB) (http.Handler, error) {
+	e := &Explorer{DB: db}
+	e.InitDBStruct()
+
+	return e, nil
+}
+
+func (e *Explorer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(r.URL.Path + "\n"))
+	w.Write([]byte(r.URL.Query().Get("admin") + "\n"))
+
+}
+
+func (e *Explorer) InitDBStruct() {
+	// Получаем список таблиц
+	rows, err := e.DB.Query("SHOW TABLES;")
+	if err != nil {
+		// Без возврата ошибки, так как паника вызывается при запуске программы
+		// потому не доставит проблем во время работы
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	tables := make([]string, 0)
+	table := ""
+
+	for rows.Next() {
+		err := rows.Scan(&table)
+		if err != nil {
+			panic(err)
+		}
+
+		tables = append(tables, table)
+	}
+
+	rows.Close()
+
+	e.Struct = make(map[string]map[string]Column)
+
+	// Получаем поля таблиц
+	for _, table := range tables {
+		// !!! Обязательная валидация имени таблицы (var table) при вызове от пользователя !!!
+		e.Struct[table] = make(map[string]Column)
+		rows, err = e.DB.Query("SHOW COLUMNS FROM " + table)
+		if err != nil {
+			panic(err)
+		}
+
+		for rows.Next() {
+			field := Column{}
+
+			err := rows.Scan(
+				&field.Field,
+				&field.Type,
+				&field.Null,
+				&field.Key,
+				&field.Default,
+				&field.Increment,
+			)
+
+			if err != nil {
+				panic(err)
+			}
+
+			e.Struct[table][field.Field] = field
+		}
+
+		rows.Close()
+	}
+
+}
