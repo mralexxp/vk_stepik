a5fb68afebe6efdc048d0bbad22a76b622b82c32
mralexxp <fl.alexaxp@gmail.com>
2025-01-29 12:16:38 +0500

    Adaptive for ID use

--- 9/99_hw/rwa/internal/storage/users/store.go
+++ 9/99_hw/rwa/internal/storage/users/store.go
@@ -2,44 +2,79 @@ package users
 
 import (
 	"fmt"
+	"log"
 	"rwa/internal/models"
 )
 
 type Store struct {
-	db map[string]*models.User
+	db         map[uint64]*models.User
+	UsernameID map[string]uint64
+	nextID     uint64
 }
 
 func NewUsersStore() *Store {
-	return &Store{db: make(map[string]*models.User)}
+	return &Store{
+		db:         make(map[uint64]*models.User),
+		UsernameID: make(map[string]uint64),
+		nextID:     1, // ID начинаются с единицы
+	}
 }
 
-func (s *Store) AddUser(user *models.User) error {
-	if _, ok := s.db[user.Username]; ok {
-		return fmt.Errorf("username '%s' already exists", user.Username)
+func (s *Store) Add(user *models.User) (uint64, error) {
+	if _, ok := s.UsernameID[user.Username]; ok {
+		return 0, fmt.Errorf("username %s already exists", user.Username)
 	}
 
-	s.db[user.Username] = user
-	return nil
+	user.ID = s.nextID
+	s.nextID++
+
+	s.UsernameID[user.Username] = user.ID
+	s.db[user.ID] = user
+
+	return user.ID, nil
 }
 
-func (s *Store) GetUser(username string) (*models.User, error) {
-	if user, ok := s.db[username]; ok {
-		return user, nil
+func (s *Store) GetByUsername(username string) (*models.User, error) {
+	if id, ok := s.UsernameID[username]; ok {
+		if u, ok := s.db[id]; ok {
+			return u, nil
+		}
+
+		log.Println("user found in relation UsernameID, but not found DB: ", username, id)
 	}
 
 	return nil, fmt.Errorf("username '%s' not found", username)
 }
 
-func (s *Store) DeleteUser(username string) error {
-	if user, ok := s.db[username]; ok {
-		delete(s.db, user.Username)
+func (s *Store) GetByID(id uint64) (*models.User, error) {
+	if u, ok := s.db[id]; ok {
+		return u, nil
+	}
+
+	return nil, fmt.Errorf("user %d not found", id)
+}
+
+func (s *Store) DeleteByUsername(username string) error {
+	if id, ok := s.UsernameID[username]; ok {
+		delete(s.UsernameID, username)
+		delete(s.db, id)
 		return nil
 	}
 
 	return fmt.Errorf("username '%s' not found", username)
 }
 
-func (s *Store) UpdateUser(user *models.User) error {
+func (s *Store) DeleteByID(id uint64) error {
+	if u, ok := s.db[id]; ok {
+		delete(s.UsernameID, u.Username)
+		delete(s.db, id)
+		return nil
+	}
+
+	return fmt.Errorf("user %d not found", id)
+}
+
+func (s *Store) Update(user *models.User) error {
 	// todo: обновляются только переданные поля
 	const op = "Store.UpdateUser"
 
