bb2983f1f02fe008d6a2c57edb17823c72ef193a
mralexxp <fl.alexaxp@gmail.com>
2025-01-29 12:15:48 +0500

    Add contentType MW

---
+++ 9/99_hw/rwa/internal/handlers/middleware.go
@@ -0,0 +1,11 @@
+package handlers
+
+import "net/http"
+
+func (h *Handlers) ContentTypeMiddleWare(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+		next.ServeHTTP(w, r)
+	})
+}
