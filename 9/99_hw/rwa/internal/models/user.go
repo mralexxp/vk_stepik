f575b213037d7c893e6c7948355d79b2fcadc59a
mralexxp <fl.alexaxp@gmail.com>
2025-01-29 12:16:46 +0500

    Adaptive for ID use

--- 9/99_hw/rwa/internal/models/user.go
+++ 9/99_hw/rwa/internal/models/user.go
@@ -5,13 +5,13 @@ type User struct {
 		Не использовал ID, так как проект работает на мапах, где получаем и отправляем юзернеймы.
 		Без использования реляционной БД это придется делать новой MAPой
 	*/
-	//ID       uint64 `json:"id"`
+	ID       uint64 `json:"id"`
 	Username string `json:"username"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
 	Created  int64  `json:"created"`
-	Updated  uint64 `json:"updated"`
+	Updated  int64  `json:"updated"`
 	Bio      string `json:"bio"`
-	// [username]
-	Follows map[string]struct{}
+	// [id]
+	Follows map[uint64]struct{}
 }
