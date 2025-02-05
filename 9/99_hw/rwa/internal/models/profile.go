4e7fb279e66084036c9164379eada7ea32f1dfb3
mralexxp <fl.alexaxp@gmail.com>
2025-02-05 10:53:33 +0500

    Add profile constructor

--- 9/99_hw/rwa/internal/models/profile.go
+++ 9/99_hw/rwa/internal/models/profile.go
@@ -5,3 +5,11 @@ type Profile struct {
 	Follow    map[uint64]struct{}
 	Followers map[uint64]struct{}
 }
+
+func NewProfile(id uint64) *Profile {
+	return &Profile{
+		ID:        id,
+		Follow:    make(map[uint64]struct{}),
+		Followers: make(map[uint64]struct{}),
+	}
+}
